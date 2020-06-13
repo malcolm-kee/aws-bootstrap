af71165249b191ebca933edee7fd1f7a0987263b
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-06-13 21:15:24 +0800

    fix deploy script output

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const { hostname } = require('os')
 const http = require('http');
 
-const message = 'Hello Cloud\n';
 const port = 8080;
 
-const server = http.createServer((_, res) => {
+const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end(message);
+    res.end(`Hello Cloud ${req.headers["user-agent"]}\n`);
 })
 
 server.listen(port, hostname, () => {
