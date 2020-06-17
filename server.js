23f6d3c17c5c080e9001bb6ab1b5ca93fd791c7a
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-06-17 10:51:24 +0800

    add access and code for s3 bucket

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
+const { S3 } = require('aws-sdk')
 const { hostname } = require('os')
 const http = require('http');
+
+const s3 = new S3({
+    apiVersion: '2006-03-01'
+});
 
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end(`Hello Cloud ${req.headers["user-agent"]}
-    from ${hostname()}
-    `);
+    s3.listBuckets((err, data) => {
+        if (err) {
+            res.statusCode = 500;
+            res.setHeader('Content-Type', 'text/plain');
+            return res.end(`Error: ${err}`)
+        }
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end(`Hello Cloud ${req.headers["user-agent"]}
+        from ${hostname()}
+        Bucket data: ${JSON.stringify(data.Buckets, null, 2)}
+        `);
+    })
 })
 
 server.listen(port, hostname, () => {
