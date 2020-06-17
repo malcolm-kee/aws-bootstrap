a0ada0e89e6e5453ae9b2e907794e4f6f5d4d012
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-06-17 15:12:17 +0800

    update textract region

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const AWS = require('aws-sdk')
 const { hostname } = require('os')
 const http = require('http');
 const fetch = require('node-fetch');
-const textract = new AWS.Textract();
+const textract = new AWS.Textract({
+    apiVersion: '2018-06-27',
+    region: 'ap-southeast-2'
+});
 
 const port = 8080;
 
@@ -15,26 +18,26 @@ const server = http.createServer((req, res) => {
     }
 
     fetch('http://www.moha.gov.my/images/maklumat_bahagian/KK/kdndomestic.pdf')
-    .then(response => {
-        return response.buffer()
-    })
-    .then(pdfBuffer => {
-        textract.analyzeDocument({
-            Document: {
-                Bytes: pdfBuffer
-            },
-            FeatureTypes: ["TABLES"]
-        }, (err, data) => {
-            if (err) {
-                return handleError(err)
-            }
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/plain');
-            return res.end(`Hello Textract: 
-            ${JSON.stringify(data.Blocks, null, 2)}`)
+        .then(response => {
+            return response.buffer()
         })
-    })
-    .catch(handleError);
+        .then(pdfBuffer => {
+            textract.analyzeDocument({
+                Document: {
+                    Bytes: pdfBuffer
+                },
+                FeatureTypes: ["TABLES"]
+            }, (err, data) => {
+                if (err) {
+                    return handleError(err)
+                }
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/plain');
+                return res.end(`Hello Textract: 
+            ${JSON.stringify(data.Blocks, null, 2)}`)
+            })
+        })
+        .catch(handleError);
 })
 
 server.listen(port, hostname, () => {
