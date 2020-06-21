7fc7cb3d080d0bd2cfb939f82a46bceff7db0cf2
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-06-21 17:12:25 +0800

    deploy lambda

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -13,9 +13,20 @@ EC2_INSTANCE_TYPE=t2.micro
 AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap --query Account --output text`
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
 TEXTRACT_BUCKET="$STACK_NAME-$REGION-textract-$AWS_ACCOUNT_ID"
+LAMBDA_CODE_BUCKET="$STACK_NAME-lambda-code-bucket"
+LAMBDA_CODE_KEY="$STACK_NAME-lambda-code"
 
-echo -e "\n\n=====================Building Lambda======================="
+echo -e "\n\n=============Building and Deploying Lambda Code============="
+rm -f dist.zip;
+rm -rf dist;
 npm run build:lambda;
+7z a dist.zip ./dist/*;
+aws s3api put-object \
+    --region $REGION \
+    --profile $CLI_PROFILE \
+    --bucket $LAMBDA_CODE_BUCKET \
+    --key $LAMBDA_CODE_KEY \
+    --body ./dist.zip
 
 echo -e "\n\n=====================Deploying setup.yml===================="
 aws cloudformation deploy \
@@ -45,7 +56,9 @@ aws cloudformation deploy \
         GitHubBranch=$GH_BRANCH \
         GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
         CodePipelineBucket=$CODEPIPELINE_BUCKET \
-        TextractBucket=$TEXTRACT_BUCKET
+        TextractBucket=$TEXTRACT_BUCKET \
+        LambdaCodeBucket=$LAMBDA_CODE_BUCKET \
+        LambdaCodeKey=$LAMBDA_CODE_KEY
 
 if [ $? -eq 0 ]; then
     aws cloudformation list-exports \
